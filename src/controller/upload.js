10b6b5719843163ea10027da167c161594c4981d
daominhngoc2003 <ngocdm22052003@gmail.com>
2023-09-27 18:54:37 +0700

    [BE] Update Image

--- src/controller/upload.js
+++ src/controller/upload.js
@@ -24,3 +24,31 @@ export const uploadImage = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const updateImage = async (req, res) => {
+  const files = req.files;
+  if (!Array.isArray(files)) {
+    return res.status(400).json({ error: "No files were uploaded" });
+  }
+
+  const publicId = req.params.publicId; // Lấy publicId của ảnh cần cập nhật
+  const newImage = req.files[0].path; // Lấy đường dẫn của ảnh mới
+
+  try {
+    // Upload ảnh mới lên Cloudinary và xóa ảnh cũ cùng lúc
+    const [uploadResult, deleteResult] = await Promise.all([
+      cloudinary.uploader.upload(newImage),
+      cloudinary.uploader.destroy(publicId),
+    ]);
+    // Trả về kết quả với url và publicId của ảnh mới
+    return res.json({
+      url: uploadResult.secure_url,
+      publicId: uploadResult.public_id,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: error.message || "Error updating image" });
+  }
+};
