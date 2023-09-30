91a9956d4d5dd971147a54475fe94112d99dc36d
daominhngoc2003 <ngocdm22052003@gmail.com>
2023-09-30 15:10:03 +0700

    [BE] Get all and Delete Attribute

--- src/routes/attribute.js
+++ src/routes/attribute.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { createAtribute } from "../controller/attribute";
+import {
+  createAtribute,
+  getAllAttribute,
+  removeAttribute,
+} from "../controller/attribute";
 const Router = express.Router();
 
+Router.get("/attributes", getAllAttribute);
+
 Router.post("/attributes", createAtribute);
+
+Router.delete("/attributes/:id", removeAttribute);
 
 export default Router;
