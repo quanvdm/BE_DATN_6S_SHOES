b065b2a3dc7d95692aa7df8ac1165e705b54b0a7
tungtokuda <tungnguyen12092003@gmail.com>
2023-10-05 22:27:04 +0700

    [BE]Update-Product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProduct, deleteProduct, getAllProduct } from "../controller/product";
+import { createProduct, deleteProduct, getAllProduct, updateProduct, } from "../controller/product";
 
 const Router = express.Router();
 
 Router.get("/products", getAllProduct);
 Router.delete("/products/:id", deleteProduct);
-Router.post("/products", createProduct)
+Router.post("/products", createProduct);
+Router.put("/products/:id", updateProduct);
 
 
 export default Router
--- src/schema/product.js
+++ src/schema/product.js
@@ -27,3 +27,33 @@ export const ProductAddSchema = Joi.object({
     "string.base": "Thương hiệu sản phẩm phải là chuỗi"
     }),
 });
+
+export const ProductUpdateSchema = Joi.object({
+  _id: Joi.string(),
+  product_name: Joi.string().max(55).optional().messages({
+    "string.empty": "Tên sản phẩm không được để trống",
+    "string.max": "Tên sản phẩm không được vượt quá {#limit} ký tự",
+  }),
+  product_code: Joi.string().max(55).optional().messages({
+    "string.empty": "Code không được để trống",
+    "string.max": "Code không được vượt quá {#limit} ký tự",
+  }),
+  product_image: Joi.array().optional(),
+  product_description_short: Joi.string().optional(),
+  product_description_long: Joi.string().optional(),
+  product_view: Joi.number().optional(),
+  favorite_id: Joi.string().optional(),
+  category_id: Joi.string().required().messages({
+    "string.empty": "Danh mục sản phẩm bắt buộc nhập",
+    "any.required": "Trường danh mục sản phẩm bắt buộc nhập",
+    "string.base": "Danh mục sản phẩm phải là chuỗi"
+  }),
+  group_id: Joi.string().optional(),
+  variant_products: Joi.array().optional(),
+  brand_id: Joi.string().required().messages({
+    "string.empty": "Thương hiệu bắt buộc nhập",
+  }),
+  slug: Joi.string(),
+  createAt: Joi.date(),
+  updateAt: Joi.date(),
+});
